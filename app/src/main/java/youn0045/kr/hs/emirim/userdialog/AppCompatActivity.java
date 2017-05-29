780d0f4aa1d5fcb4d8a41e25962d3b53c9f2e514
justhis01010101 <offentsive@naver.com>
2017-05-29 20:55:05 +0900

    #2

---
+++ app/src/main/java/youn0045/kr/hs/emirim/userdialog/AppCompatActivity.java
@@ -0,0 +1,12 @@
+package youn0045.kr.hs.emirim.userdialog;
+
+import android.os.Bundle;
+
+/**
+ * Created by appcreator16 on 2017. 5. 29..
+ */
+
+class AppCompatActivity {
+    public void onCreate(Bundle savedInstanceState) {
+    }
+}
--- app/src/main/java/youn0045/kr/hs/emirim/userdialog/MainActivity.java
+++ app/src/main/java/youn0045/kr/hs/emirim/userdialog/MainActivity.java
@@ -1,14 +1,37 @@
 package youn0045.kr.hs.emirim.userdialog;
 
+import android.graphics.Color;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button but = (Button) findViewById(R.id.but_information);
+        but.setOnClickListener(this);
 
+    }
+
+    @Override
+    public void onClick(View view) {
+        View dlgv= View.inflate(this,R.layout.dialog, null);
+        AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+        dialog.setTitle("저즈디즈 잘셍겼당");
+        dialog.setIcon(R.drawable.star);
+        dialog.setView(dlgv);
+        dialog.setPositiveButton("ㅇㅈ", null);
+        dialog.setNegativeButton("울 귀요미", null);
+        dialog.show();
     }
 }
